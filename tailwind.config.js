ff1ef4c658af644620a0d17f5ad15979887c9c96
Hallef Santos <hallefsantos@gmail.com>
2021-07-10 08:50:03 -0300

    Install font and add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
+    fontFamily: {
+      'sans': ['Space Mono', ...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   variants: {
